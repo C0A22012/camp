8f9a4061105633161e103c0e45a4692be7b0b4c0
Hiratani Shun <c0a22121a0@edu.teu.ac.jp>
2023-12-14 18:45:30 +0900

    クエリパラメータの基盤作成

--- back_end/main.py
+++ back_end/main.py
@@ -2,7 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# word = "りんご"
+camps = {"りんご":"ringo"}
 
+#クエリパラメータ
+@app.get("/camp")
+async def camp(word:str):
+    if word:
+        return (f"param:{camps[word]}")
+    else:
+        return {"none"}
+    
+# 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
