7964b2224a294c6ac0be4cc40af687bd15b2b220
IZUMI <c0a22012d6@edu.teu.ac.jp>
2023-12-21 17:41:08 +0900

    検索画面

--- front_end/A.js
+++ front_end/A.js
@@ -1,14 +1,16 @@
+inu = "aiue"
 async function callApi() {
     let element = document.getElementById("search-text");
     console.log(element.value);
-    const res = await fetch('http://127.0.0.1:8000word=' + element.value)
-        .then(result => result.json())
-        .then((output) => {
-            console.log('Output: ', output);
-            let tmpVal = output["date"];
-            console.log( String(tmpVal) );
-        }).catch(err => console.error(err));
-};
     
-    // const res = await fetch('http://127.0.0.1:8000')
-    //   .then(result => result.json())
+    const res = await fetch('http://127.0.0.1:8000?word=' + element.value)
+    .then((data) => {
+        deta.json() // {"result": ""}
+        const s = document.querySelector('.s');
+        s.innerText =data.json()["result"];
+        
+    })
+};
+
+// const s = document.querySelector('.s');
+// s.innerText = "Hello!";
